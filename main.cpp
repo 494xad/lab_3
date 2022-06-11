94e20daacdc6918e4fc74ee01c00e74a84ead03b
494xad <494xad@mail.ru>
2022-06-11 13:42:39 +0700

    Вывод списка файлов
    Реализован вывод списка файлов в рабочей директории в два окна.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include "qfiledialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "qtreeview.h"
+#include "qfilesystemmodel.h"
+#include "qtableview.h"
+#include "qboxlayout.h"
+
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+
 {
-    ui->setupUi(this);
+    QFileSystemModel *model = new QFileSystemModel();
+    model->setRootPath(QDir::currentPath());
+    QTreeView *treeView = new QTreeView(this);
+    treeView->setModel(model);
+    treeView->setRootIndex(model->index(QDir::currentPath()));
+    QTableView *table = new QTableView(this);
+    table->setModel(model);
+    table->setRootIndex(model->index(QDir::currentPath()));
+    QBoxLayout *boxLayout = new QBoxLayout(QBoxLayout::Direction::LeftToRight, this);
+    boxLayout->addWidget(treeView);
+    boxLayout->addWidget(table);
+    QWidget *test = new QWidget(this);
+    test->setLayout(boxLayout);
+    //this->setLayout(boxLayout);
+    this->setCentralWidget(test);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
